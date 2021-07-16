039172368cc76b46a8d5248b1cec28c3518a208c
Kirill <kirillnemkov98@gmail.com>
2021-07-16 15:17:43 +0300

    cardlist

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import { Route, Switch, Redirect } from 'react-router-dom'
 import ActivationSuccess from './components/ActivationSuccess/ActivationSuccess'
+import CardList from './components/CardList/CardList.jsx'
 import SignupSuccessMessage from './components/Signup-success-message/Signup-success-message'
 
 function App() {
     return (
-        <Switch>
-            <Route exact path="/activation/success/:link">
-                <ActivationSuccess />
-            </Route>
-            <Route exact path="/signUp/success">
-                <SignupSuccessMessage />
-            </Route>
-        </Switch>
+      <CardList/>
+        // <Switch>
+        //     <Route exact path="/activation/success/:link">
+        //         <ActivationSuccess />
+        //     </Route>
+        //     <Route exact path="/signUp/success">
+        //         <SignupSuccessMessage />
+        //     </Route>
+        // </Switch>
     )
 }
 
--- client/src/http/axios.config.js
+++ client/src/http/axios.config.js
@@ -11,31 +11,31 @@ $api.interceptors.request.use((config) => {
     return config
 })
 
-$api.interceptors.request.use(
-    (config) => {
-        return config
-    },
-    async((error) => {
-        const originalRequest = error.config
-        if (
-            error.response.status == 401 &&
-            error.config &&
-            !error.config._isRetry
-        ) {
-            originalRequest._isRetry = true
-            try {
-                const response = await axios.get(
-                    `${host}/api/v1/auth/refresh`,
-                    { withCredentials: true }
-                )
-                const { accessToken } = response.data
-                localStorage.setItem('token', accessToken)
-                return $api.request(originalRequest)
-            } catch (error) {
-                console.log('Не авторизован')
-            }
-        }
-        throw error
-    })
-)
+// $api.interceptors.request.use(
+//     (config) => {
+//         return config
+//     },
+//     async((error) => {
+//         const originalRequest = error.config
+//         if (
+//             error.response.status == 401 &&
+//             error.config &&
+//             !error.config._isRetry
+//         ) {
+//             originalRequest._isRetry = true
+//             try {
+//                 const response = await axios.get(
+//                     `${host}/api/v1/auth/refresh`,
+//                     { withCredentials: true }
+//                 )
+//                 const { accessToken } = response.data
+//                 localStorage.setItem('token', accessToken)
+//                 return $api.request(originalRequest)
+//             } catch (error) {
+//                 console.log('Не авторизован')
+//             }
+//         }
+//         throw error
+//     })
+// )
 export default $api
